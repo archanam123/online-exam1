d15e8d60695e0e1a44223fe568d2c1b308933878
Patil <archana.c.patil@accenture.com>
2020-05-30 15:57:47 +0530

    reverting code

--- sample.java
+++ sample.java
@@ -2,11 +2,6 @@ import java.awt.*;
 import java.awt.event.*;
 import java.sql.*;
 
-st.executeQuery("INSERT into student values("+id+",'"+name+"',"+phone+",'"+pwd+"')");
-st.executeQuery("SELECT accountNumber, balance FROM accounts WHERE account_owner_id = " + request.getParameter("user_id")");
-st.executeQuery("UPDATE question set q_id="+qid+",ques='"+quest+"',ans="+ans+",author='"+author+"',op1='"+op1+"',op2='"+op2+"',op3='"+op3+"',op4='"+op4+"' where q_id="+qid");
-st.executeQuery("DELETE from question where q_id="+qid);
-
 public class sample extends Frame implements WindowListener,ActionListener
 {
 public void windowActivated(WindowEvent e) {  }
